58b10ea1359e88ff07e59691afab15a4e9169cb8
vagrant <vagrant@master>
2024-01-07 17:36:07 +0000

    Repositorio - kubernetes

--- 18-Monitorizacion-Optimizacion/comandos_vpa-limpieza.sh
+++ 18-Monitorizacion-Optimizacion/comandos_vpa-limpieza.sh
@@ -4,4 +4,4 @@ kubectl delete -f deployment_complex-vpa.yml
 kubectl delete -f vpa_complex-autoscaler.yml
 kubectl delete -f deployment_basic-vpa.yml
 kubectl delete -f vpa_basic-autoscaler.yml
-~/vpa/autoscaler/vertical-pod-autoscaler/hack/vpa-down.sh
+~/download/vpa/autoscaler/vertical-pod-autoscaler/hack/vpa-down.sh
