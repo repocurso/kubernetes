faf3b65f1c12b882548351fd8ed2281fd09778b9
Repositorio Cursos <152635234+repocurso@users.noreply.github.com>
2024-01-08 19:22:46 +0100

    Repositorio - kubernetes

--- 00-Preparacion/comandos_prepare.sh
+++ 00-Preparacion/comandos_prepare.sh
@@ -5,5 +5,5 @@ ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -N "" <<< y
 ssh-copy-id 192.168.100.11 -f 
 ssh-copy-id 192.168.100.12 -f
 
-sudo sed -i '$a 192.168.100.10\tmaster\tmaster\n192.168.100.11\tnode1\tnode1\n192.168.100.12\tnode2\tnode2' /etc/hosts
-
+sudo sed -i '/master/d; /node1/d; /node2/d' /etc/hosts
+sudo sed -i '$a 192.168.100.10\tmaster.local\tmaster\n192.168.100.11\tnode1.local\tnode1\n192.168.100.12\tnode2.local\tnode2' /etc/hosts
